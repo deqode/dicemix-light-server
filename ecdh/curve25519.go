609c0d65bf3591dec8d48d68637d0363b0c34202
Manjeet Thadani <mthandani@grepruby.com>
2018-08-23 20:21:49 +0530

    Removed unused code

--- ecdh/curve25519.go
+++ ecdh/curve25519.go
@@ -18,31 +18,18 @@ func NewCurve25519ECDH() ECDH {
 	return &curve25519ECDH{}
 }
 
-// Marshal converts crypto.PublicKey into byte[]
-func (e *curve25519ECDH) Marshal(p crypto.PublicKey) []byte {
-	var ecdhCurve = ecdh.NewCurve25519ECDH()
-	return ecdhCurve.Marshal(p)
-}
-
 // Unmarshal converts byte[] to crypto.PublicKey
 func (e *curve25519ECDH) Unmarshal(data []byte) (crypto.PublicKey, bool) {
 	var ecdhCurve = ecdh.NewCurve25519ECDH()
 	return ecdhCurve.Unmarshal(data)
 }
 
-// Marshal converts crypto.PrivateKey into byte[]
-// used in KESK stage
-func (e *curve25519ECDH) MarshalSK(p crypto.PrivateKey) []byte {
-	pri := p.(*[32]byte)
-	return pri[:]
-}
-
+// Unmarshal converts byte[] to crypto.PrivateKey
 func (e *curve25519ECDH) UnmarshalSK(data []byte) (crypto.PrivateKey, bool) {
 	var pri [32]byte
 	if len(data) != 32 {
 		return nil, false
 	}
-
 	copy(pri[:], data)
 	return &pri, true
 }
