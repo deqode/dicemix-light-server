91efb3a8d536b91110d0fa2ba5eb7b84791055da
Manjeet Thadani <mthandani@grepruby.com>
2018-08-24 15:51:33 +0530

    Added Is Subset function

--- utils/utils.go
+++ utils/utils.go
@@ -10,14 +10,27 @@ import (
 	"github.com/shomali11/util/xhashes"
 )
 
-// ContainsHash - returns true if (hash ∈ roots)
-func ContainsHash(hash uint64, roots []uint64) bool {
-	for _, root := range roots {
-		if hash == root {
-			return true
+// Subset returns true if the first array is completely
+// contained in the second array. There must be at least
+// the same number of duplicate values in second as there
+// are in first.
+func IsSubset(first, second []uint64) bool {
+	set := make(map[uint64]int)
+	for _, value := range second {
+		set[value]++
+	}
+
+	for _, value := range first {
+		value = Reduce(value)
+		if count, found := set[value]; !found {
+			return false
+		} else if count < 1 {
+			return false
+		} else {
+			set[value] = count - 1
 		}
 	}
-	return false
+	return true
 }
 
 // EqualBytes - compares two slice's of [][]byte for equality
