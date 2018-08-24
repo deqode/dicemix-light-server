14f721f334feb4272df76092bd88d679e9853754
Manjeet Thadani <mthandani@grepruby.com>
2018-08-24 17:12:34 +0530

    Removed unused functions

--- solver/solver_flint.cpp
+++ solver/solver_flint.cpp
@@ -240,12 +240,3 @@ void *allocArgv(int argc)
 {
   return malloc(sizeof(char *) * argc);
 }
-
-void printArgs(int argc, char **argv)
-{
-  int i;
-  for (i = 0; i < argc; i++)
-  {
-    printf("%s\n", argv[i]);
-  }
-}
--- solver/solver_flint.h
+++ solver/solver_flint.h
@@ -7,7 +7,6 @@ extern "C"
 #endif
 
   char ** solve(int n, char **const out_messages, const char *prime, const char **const sums);
-  void printArgs(int argc, char **argv);
   void *allocArgv(int argc);
 #ifdef __cplusplus
 }
